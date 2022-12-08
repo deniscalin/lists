155e16830e1c29e54131c8bed0db0e97279a7507
deniscalin <denis.calin@braze.com>
2022-12-08 10:28:04 -0800

    Rename 2 main  files, add folders to gitignore, Day 16

--- gui.py
+++ gui.py
@@ -5,6 +5,6 @@ label = sg.Text("Control your day! Add your todo.")
 inputbox = sg.InputText(tooltip="Enter todo")
 add_btn = sg.Button("Add")
 
-window = sg.Window('nulist List Service', layout=[[label], [inputbox, add_btn]])
+window = sg.Window('Welcome to Nulist Idea Mapping Service', layout=[[label], [inputbox, add_btn]])
 window.read()
 window.close()
