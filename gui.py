13b57f8b86a5d3b2354771b9734ce6df215bafc3
deniscalin <denis.calin@braze.com>
2022-12-08 14:48:49 -0800

    Implement Add button

--- gui.py
+++ gui.py
@@ -2,9 +2,23 @@ import functions
 import PySimpleGUI as sg
 
 label = sg.Text("Control your day! Add your todo.")
-inputbox = sg.InputText(tooltip="Enter todo")
+inputbox = sg.InputText(tooltip="Enter todo", key="todo")
 add_btn = sg.Button("Add")
 
-window = sg.Window('Welcome to Nulist Idea Mapping Service', layout=[[label], [inputbox, add_btn]])
-window.read()
+window = sg.Window('Welcome to Nulist Idea Mapping Service',
+                   layout=[[label], [inputbox, add_btn]],
+                   font=('Andale Mono', 20))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case 'Add':
+            todos = functions.get_todos()
+            todos.append(values["todo"] + "\n")
+            functions.write(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
