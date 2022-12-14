4e7c21b4e628702db17e370d9c6c842d20577736
deniscalin <denis.calin@braze.com>
2022-12-13 22:36:46 -0800

    Implement adding new items to the list

--- web.py
+++ web.py
@@ -4,6 +4,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write(todos)
+
+
 st.title("Nulist Idea Mapping Service")
 #now = time.strftime("%b %m, %Y %H:%M")
 st.subheader("Control your day! Add todo.")
@@ -11,5 +18,8 @@ st.subheader("Control your day! Add todo.")
 for item in todos:
     st.checkbox(item)
 
-st.text_input(label="", placeholder="Add todo")
+st.text_input(label="", placeholder="Add todo", on_change=add_todo,
+              key="new_todo")
 
+
+st.session_state
